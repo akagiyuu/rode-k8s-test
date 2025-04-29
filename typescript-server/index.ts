778e4f85c82e21f15cd27f60be599c40e83e408b
TCO46 <patohru2006@gmail.com>
2025-04-29 21:07:48 +0700

    feat: setup sender

--- typescript-server/index.ts
+++ typescript-server/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import amqp from 'amqplib/callback_api'
+
+amqp.connect('amqp://localhost:5672', function(err, conn) {
+	if(err) throw err;
+
+	conn.createChannel(function(err, channel) {
+		if (err) throw err;
+		
+		var queue = 'Date';
+		var msg = new Date();
+ 
+		channel.assertQueue(queue, {
+			durable: false
+		});
+
+		channel.sendToQueue(queue, Buffer.from(msg));
+		console.log(`[x] Sent ${msg}`)
+
+		setTimeout(() => {
+			conn.close();
+			process.exit(0)
+		}, 500)
+	})
+})
