86ecb39917f93c6f1f616783dc96831e0846710b
TCO46 <patohru2006@gmail.com>
2025-05-01 12:35:34 +0700

    feat: k8s for typescript

--- typescript-server/index.ts
+++ typescript-server/index.ts
@@ -1,20 +1,26 @@
 import amqp from 'amqplib/callback_api'
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
-amqp.connect('amqp://localhost:5672', function(err, conn) {
+amqp.connect(process.env.AMQP_ADDR, function(err, conn) {
 	if(err) throw err;
 
-	conn.createChannel(function(err, channel) {
+	conn.createChannel(async function(err, channel) {
 		if (err) throw err;
 		
 		var queue = process.env.QUEUE_NAME;
-		var msg = new Date();
  
 		channel.assertQueue(queue, {
 			durable: false
 		});
-
-		channel.sendToQueue(queue, Buffer.from(msg));
-		console.log(`[x] Sent ${msg}`)
+		
+		while(true) {
+			var msg = new Date();
+			await delay(10000);
+			channel.sendToQueue(queue, Buffer.from(msg));
+			console.log(`[x] Sent ${msg}`)
+		}
 
 		setTimeout(() => {
 			conn.close();
